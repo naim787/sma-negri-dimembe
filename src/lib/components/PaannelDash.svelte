33d58cda07926aa761bdb254c19a8f8fec34defe
naim787 <naimmmmab@gmail.com>
2025-09-17 09:06:41 -0400

    Update: 9/17/2025, 9:06:41 AM

--- src/lib/components/PaannelDash.svelte
+++ src/lib/components/PaannelDash.svelte
@@ -4,7 +4,7 @@
     export let Bars;
 </script>
 
-<div class={`w-82 h-full bg-white flex justify-center items-start lg:static absolute left-0 transform ${Bars ? "translate-x-[-100%]" : "translate-x-[-100%]"} `}>
+<div class={`w-82 h-full bg-white flex justify-center items-start lg:static absolute left-0 transform ${Bars ? "translate-x-[-100%]" : "translate-x-[0%]"} `}>
             <ul class="w-[85%] h-2/3 p-2">
                 <Accordion flush>
                     <AccordionItem>
