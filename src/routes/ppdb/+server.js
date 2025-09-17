b160b4164eb2c6d41045c5b80c633e3fa9aa1d1c
naim787 <naimmmmab@gmail.com>
2025-09-17 10:05:59 -0400

    Update: 9/17/2025, 10:05:59 AM

--- src/routes/ppdb/+server.js
+++ src/routes/ppdb/+server.js
@@ -5,6 +5,4 @@ export function GET() {
    let data = DesDat("f545d873d98301bd2f33952d3aff3a8e10bb4b9afacfc3620ef8cf534483119f", "123")
     return json({message : data});
 }
-
-
 // f545d873d98301bd2f33952d3aff3a8e10bb4b9afacfc3620ef8cf534483119f
