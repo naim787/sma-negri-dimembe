c3e1eb1f31a9a1b44d19552a0398efa337dade30
naim787 <naimmmmab@gmail.com>
2025-09-17 09:34:23 -0400

    Update: 9/17/2025, 9:34:23 AM

--- src/lib/components/PaannelDash.svelte
+++ src/lib/components/PaannelDash.svelte
@@ -20,7 +20,7 @@
                     <a href="/" class="w-full h-16 flex justify-start items-center text-gray-500">
                         <h1 class="">Siswa</h1>
                     </a>
-                    
+                    <DarkMode />
                 </Accordion>
             </ul>
 </div>
