9b38088ddefeddb17ca872b1724a9152effe9f60
naim787 <naimmmmab@gmail.com>
2025-09-17 09:46:55 -0400

    Update: 9/17/2025, 9:46:55 AM

--- src/routes/ppdb/+page.svelte
+++ src/routes/ppdb/+page.svelte
@@ -56,9 +56,9 @@
         <!-- <h1 class="text-3xl tcext-red-500 m-auto">{message}</h1> -->
          <div class="w-full bg-gray-200 dark:bg-black flex flex-col justify-start items-start p-2"
           role="button"
-  tabindex="0"
-  on:click={() => Bars = true}
-  on:keypress={(e) => { if (e.key === 'Enter') Bars = true; }}
+          tabindex="0"
+          on:click={() => Bars = true}
+          on:keypress={(e) => { if (e.key === 'Enter') Bars = true; }}
          >
             <div class="w-full p-3"><h1 class="text-4xl font-bold gloria">Dashbord</h1></div>
             <div class="p-3 bg-white rounded-2xl">
