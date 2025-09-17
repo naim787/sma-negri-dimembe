f0c1067dc52a227a77e4b1ea12374ce7e42fbfc3
naim787 <naimmmmab@gmail.com>
2025-09-17 09:39:55 -0400

    Update: 9/17/2025, 9:39:55 AM

--- src/routes/ppdb/+page.svelte
+++ src/routes/ppdb/+page.svelte
@@ -50,7 +50,7 @@
 
 <!-- <Nav /> -->
  <NavvDash on:panelDash={() => {Bars = !Bars}} />
-   <button class="" on:click={()=> Bars }>
+   <button class="" on:click={()=> Bars = }>
      <div class="w-full h-[90vh] flex bg-gray-200">
          <!-- navbar -->
         <PaannelDash Bars={Bars} />
