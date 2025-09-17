b35654bc7a43c0cedacd21d03f98346548ced38a
naim787 <naimmmmab@gmail.com>
2025-09-17 08:10:47 -0400

    Update: 9/17/2025, 8:10:47 AM

--- src/lib/components/PaannelDash.svelte
+++ src/lib/components/PaannelDash.svelte
@@ -16,7 +16,7 @@
                     <a href="/" class="w-full h-16 flex justify-start items-center text-gray-500">
                         <h1 class="">Siswa</h1>
                     </a>
-                    
+                    Acc
                 </Accordion>
             </ul>
 </div>
