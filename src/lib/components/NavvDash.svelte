cce06592c5a1d16a954df9c67af0db6e6a918c0e
naim787 <naimmmmab@gmail.com>
2025-09-17 09:55:00 -0400

    Update: 9/17/2025, 9:55:00 AM

--- src/lib/components/NavvDash.svelte
+++ src/lib/components/NavvDash.svelte
@@ -8,7 +8,7 @@
   let activeUrl = $derived(page.url.pathname);
 </script>
 
-<Navbar class="border-b-1  dark:bg-gray-800 ">
+<Navbar class="border-b-1 dark:bg-gray-800 ">
   <NavBrand href="/">
     <img src="./favicon.webp" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
     <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">SMANDIM</span>
