85eb0f2e2abe15875f07f9bedcbd9968beeb5a57
naim787 <naimmmmab@gmail.com>
2025-09-17 08:41:56 -0400

    Update: 9/17/2025, 8:41:56 AM

--- src/routes/ppdb/+page.svelte
+++ src/routes/ppdb/+page.svelte
@@ -51,7 +51,7 @@
 </script>
 
 <!-- <Nav /> -->
- <NavvDash on:panelDash={() => {}} />
+ <NavvDash on:panelDash={() => {b}} />
     <div class="w-full h-[90vh] flex bg-gray-200">
         <!-- navbar -->
        <PaannelDash />
