713eaf7717c35175bea87280084d835f32cc29a5
naim787 <naimmmmab@gmail.com>
2025-10-08 01:10:19 -0400

    Update: 10/8/2025, 1:10:19 AM

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -29,6 +29,7 @@
     <NavLi class="text-black" href="/maintenance">Kurikulum</NavLi>
     <NavLi class="text-black" href="/gallery">Gallery</NavLi>
     <NavLi class="text-black" href="/maintenance">Kontak</NavLi>
+    <NavLi class="text-black bg-white font-bold" href="/ppdb">PPDB</NavLi>
     <!-- <NavLi class="text-black p-3 bg-yellow-500" href="/ppdb">PPDB</NavLi> -->
   </NavUl>
 </Navbar>
