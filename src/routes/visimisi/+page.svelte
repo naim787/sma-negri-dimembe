ea6b381a95a027e6496354ae94428c57646f4a0b
naim787 <naimmmmab@gmail.com>
2025-10-07 23:59:29 -0400

    Update: 10/7/2025, 11:59:29 PM

---
+++ src/routes/visimisi/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Footer from "$lib/components/Footer.svelte";
+  import Nav from "$lib/components/Nav.svelte";
+
+</script>
+
+<Nav />
+    <div class="w-[100vw] h-[100vh]">
+        
+    </div>
+<Footer />
