4fc46042181c75d36b35f58a44a58233ad76235a
naim787 <naimmmmab@gmail.com>
2025-10-07 22:39:01 -0400

    Update: 10/7/2025, 10:39:01 PM

--- src/lib/components/L3.svelte
+++ src/lib/components/L3.svelte
@@ -15,7 +15,7 @@
 </script>
 
 <div class="w-full h-auto flex flex-col">
-    <div class="w-full h-[50vh] bg-blue-100 p-2 flex flex-wrap justify-evenly items-center gap-1">
+    <div class="w-full h-auto bg-blue-100 px-2 py-10 flex flex-wrap justify-evenly items-center gap-1">
         <div class="w-full"><h1 class="text-5xl text-center gloria">Extrakurikuler</h1></div>
         <div class="w-40 h-30 rounded-xl bg-blue-950 flex items-center justify-center">
             <DribbbleSolid class="shrink-0 h-15 w-15 text-yellow-500" />
@@ -30,6 +30,25 @@
             <h1 class="text-2xl text-white">Teater</h1>
         </div>
         <div class="w-40 h-30 rounded-xl bg-blue-950 flex items-center justify-center">
+            <MicrophoneSolid class="shrink-0 h-15 w-15 text-green-500" />
+            <h1 class="text-2xl text-white">Menyayi</h1>
+        </div>
+    </div>
+    <div class="w-full h-auto bg-blue-100 px-2 py-10 flex flex-wrap justify-evenly items-center gap-1">
+      <div class="w-full"><h1 class="text-5xl text-center gloria">Extrakurikuler</h1></div>
+        <div class="w-full h-30 rounded-xl bg-blue-950 flex items-center justify-center">
+            <DribbbleSolid class="shrink-0 h-15 w-15 text-yellow-500" />
+            <h1 class="text-2xl text-white">Bola Folly</h1>
+        </div>
+        <div class="w-full h-30 rounded-xl bg-blue-950 flex items-center justify-center">
+            <LifeSaverSolid class="shrink-0 h-15 w-15 text-indigo-500" />
+            <h1 class="text-2xl text-white">Bola Kaki</h1>
+        </div>
+        <div class="w-full h-30 rounded-xl bg-blue-950 flex items-center justify-center">
+            <CandyCaneSolid class="shrink-0 h-15 w-15 text-red-500" />
+            <h1 class="text-2xl text-white">Teater</h1>
+        </div>
+        <div class="w-full h-30 rounded-xl bg-blue-950 flex items-center justify-center">
             <MicrophoneSolid class="shrink-0 h-15 w-15 text-green-500" />
             <h1 class="text-2xl text-white">Menyayi</h1>
         </div>
